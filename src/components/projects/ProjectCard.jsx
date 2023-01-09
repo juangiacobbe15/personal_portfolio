20779733fb2a381350abb43990d5ee35355ba154
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2023-01-09 17:09:28 -0300

    reworked projects section

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,15 +1,17 @@
+import html from '../../assets/html.png'
+
 import '../../styles/global.css';
 import '../../styles/projectCard.css';
 
 export const ProjectCard = () => {
     return (
-        <div className="project-card">
-            <div className="project-img">
-                <img src="https://tse2.mm.bing.net/th?id=OIP.kEUkm4rRg7FZZYD19PRuwAHaE8&pid=Api&P=0" alt="project" />
+        <div className="project-card d-flex  justify-content-around flex-row">
+            <div>
+                <img src={html} alt="project" className='img'/>
             </div>
             <div className="project-text">
-                <p className='project-title'>project 1</p>
-                <p className="project-des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, provident.</p>
+                <p className='project-title p-2'>project 1</p>
+                <p className="project-des p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, provident.</p>
                 <button className='project-btn'>view in github</button>
             </div>
         </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,20 +2,34 @@ import { ProjectCard } from './ProjectCard';
 
 import '../../styles/global.css';
 import '../../styles/projects.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export const Projects = () => {
     return (
-        <section className="projects" name='projects'>
+        // <section className="projects" name='projects'>
+            // <div className="section-title-container">
+            //     <h1 className="section-title">My recent projects</h1>
+            //     <p className="section-subtitle">There are some of my recent projects</p>
+            // </div>
+
+        //     <div className="projects-grid" data-aos='fade-in' data-aos-delay='500'>
+        //         <ProjectCard />
+        //         <ProjectCard />
+        //         <ProjectCard />
+        //     </div>
+        // </section>
+
+        <Container fluid className='d-flex justify-content-center align-center flex-column text-center' name='projects'>
             <div className="section-title-container">
-                <h1 className="section-title">My recent projects</h1>
-                <p className="section-subtitle">There are some of my recent projects</p>
+                    <h1 className="section-title">My recent projects</h1>
+                    <p className="section-subtitle">There are some of my recent projects</p>
             </div>
 
-            <div className="projects-grid" data-aos='fade-in' data-aos-delay='500'>
+            <Container className='d-flex justify-content-center align-center flex-column text-center'>
                 <ProjectCard />
                 <ProjectCard />
                 <ProjectCard />
-            </div>
-        </section>
+            </Container>
+        </Container>
     );
 }
